6ea574c13aaed0e2a700bcf790eb87302f458a01
stkuper <stklipper@gmail.com>
2023-04-28 22:40:29 +0300

    Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+    private int sizeOfEmpty = 10;
+    public String surname;
+    String name;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
