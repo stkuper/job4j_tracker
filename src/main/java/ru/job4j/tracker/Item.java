b1e00cc12384acb4f33650461d05fd865498cdf9
stkuper <stklipper@gmail.com>
2023-05-05 00:03:14 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+
+    LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -14,6 +18,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
