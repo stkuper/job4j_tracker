968a4efeebd03b4be62aa4a36fd68bfa12f715f4
stkuper <stklipper@gmail.com>
2023-05-07 16:27:33 +0300

    3. Массивы и модели. [#174892]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -21,7 +21,7 @@ public class Library {
         }
         System.out.println();
         for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals("Clean code")) {
+            if ("Clean code".equals(books[i].getName())) {
                 System.out.println("The book \"" + books[i].getName() + "\" consist of "
                                    + books[i].getPages() + " pages.");
             }
