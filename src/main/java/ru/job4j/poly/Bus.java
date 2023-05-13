46e00654e9ba74d5ab1c6cc041779ef26143c827
stkuper <stklipper@gmail.com>
2023-05-13 13:50:54 +0300

    4.2. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,10 +3,12 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void go() {
+        System.out.println("Автобус едит");
     }
 
     @Override
     public void passenger(int passengers) {
+        System.out.println("В автобусе " + passengers + " пассажиров");
     }
 
     @Override
