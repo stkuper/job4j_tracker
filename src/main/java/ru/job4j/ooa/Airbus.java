52b65103272267f601fe22676d7a2d455d08d123
stkuper <stklipper@gmail.com>
2023-05-20 22:02:24 +0300

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,39 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+        System.out.println(this.toString());
+        this.printModel();
+        this.printCountEngine();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        int engine = "A380".equals(getName()) ? 4 : COUNT_ENGINE;
+        System.out.println("Количество двигателей равно: " + engine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -0,0 +1,12 @@
+package ru.job4j.ooa;
+
+public class Airport {
+    public static void main(String[] args) {
+        Airbus airbus = new Airbus("A320");
+        airbus = new Airbus("A380");
+        airbus.setName("A380");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+    }
+}
