574737747bfacd5c238ca1021ea6bbe0802134eb
stkuper <stklipper@gmail.com>
2023-05-20 11:59:50 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -30,12 +30,15 @@ public class ValidateInputTest {
     @Test
     public void whenManyValidInput() {
         Output out = new StubOutput();
-        Input in = new StubInput(new String[]{
-                "1", "1", "1"
-        });
+        String[] numbers = new String[]{
+                "0", "1", "2"
+        };
+        Input in = new StubInput(numbers);
         ValidateInput input = new ValidateInput(out, in);
-        int selected = input.askInt("Enter menu: ");
-        assertThat(selected).isEqualTo(1);
+        for (String num : numbers) {
+            int selected = input.askInt("Enter menu: ");
+            assertThat(selected).isEqualTo(Integer.parseInt(num));
+        }
     }
 
     @Test
