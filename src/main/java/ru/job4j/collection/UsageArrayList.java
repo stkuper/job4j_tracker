f0b717f2d35979510451e18efd7fe5dbc6bb08ad
stkuper <stklipper@gmail.com>
2023-05-25 23:06:41 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Petr");
+        list.add("Ivan");
+        list.add("Stepan");
+        list.add("Sveta");
+        list.add("Elena");
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+}
