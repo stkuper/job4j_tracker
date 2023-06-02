e5b938bb3538a6cebdf2c5ecbda72ce7480e5883
stkuper <stklipper@gmail.com>
2023-06-02 11:04:17 +0300

    1. Организовать сортировку User [#10034  [#10034 #420005]]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -30,10 +30,7 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        int compareName = this.name.compareTo(o.name);
-        if (compareName == 0) {
-            return Integer.compare(this.age, o.age);
-        }
-        return compareName;
+        int result = this.name.compareTo(o.name);
+        return result != 0 ? result : Integer.compare(this.age, o.age);
     }
 }
