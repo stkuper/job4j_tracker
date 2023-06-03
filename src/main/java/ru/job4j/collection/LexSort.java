d2a5453392177e23372899197c816ffe67b6cb74
stkuper <stklipper@gmail.com>
2023-06-03 15:19:36 +0300

    4. Сортировка номера [#221284  [#221284 #420393]]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,19 +5,8 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String[] lefts = left.split(". ");
-        String[] rights = right.split(". ");
-        for (int i = 0; i < lefts.length; i++) {
-            if (isNumber(lefts[i]) && isNumber(rights[i])) {
-                int leftNum = Integer.parseInt(lefts[i]);
-                int rightNum = Integer.parseInt(rights[i]);
-                return Integer.compare(leftNum, rightNum);
-            }
-        }
-        return left.compareTo(right);
-    }
-
-    public static boolean isNumber(String str) {
-        return str.matches("-?\\d+");
+        String[] lefts = left.split("\\.");
+        String[] rights = right.split("\\.");
+        return Integer.compare(Integer.parseInt(lefts[0]), Integer.parseInt(rights[0]));
     }
 }
