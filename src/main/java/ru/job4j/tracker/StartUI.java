8787b095011137fcc8db298c81f6c11edb770ed5
stkuper <stklipper@gmail.com>
2024-02-19 22:43:53 +0300

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,7 +1,9 @@
 package ru.job4j.tracker;
 
 import ru.job4j.tracker.action.CreateAction;
+import ru.job4j.tracker.action.CreateManyItems;
 import ru.job4j.tracker.action.DeleteAction;
+import ru.job4j.tracker.action.DeleteAllItems;
 import ru.job4j.tracker.action.EditAction;
 import ru.job4j.tracker.action.ExitAction;
 import ru.job4j.tracker.action.FindItemByIdAction;
@@ -15,7 +17,6 @@ import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
 import ru.job4j.tracker.store.Store;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class StartUI {
@@ -50,14 +51,17 @@ public class StartUI {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
         Store tracker = new SqlTracker();
-        List<UserAction> actions = new ArrayList<>();
-        actions.add(new CreateAction(output));
-        actions.add(new ShowAllAction(output));
-        actions.add(new EditAction(output));
-        actions.add(new DeleteAction(output));
-        actions.add(new FindItemByIdAction(output));
-        actions.add(new FindItemByNameAction(output));
-        actions.add(new ExitAction());
+        List<UserAction> actions = List.of(
+                new CreateAction(output),
+                new ShowAllAction(output),
+                new EditAction(output),
+                new DeleteAction(output),
+                new FindItemByIdAction(output),
+                new FindItemByNameAction(output),
+                new ExitAction(),
+                new CreateManyItems(output),
+                new DeleteAllItems(output)
+        );
         new StartUI(output).init(input, tracker, actions);
     }
 }
